f790cfce018129f5b622b1add7260d7495f77475
Justin Xue <tsunyi@users.noreply.github.com>
2021-11-12 18:35:20 +0800

    Add Xtron Pro additional buttons, C and start

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+namespace controller {
+    //% fixedInstance whenUsed block="{id:controller}C"
+    export const C = new Button(8, DAL.CFG_PIN_BTN_MENU2);
+    //% fixedInstance whenUsed block="{id:controller}start"
+    export const start = new Button(9, 94);
+}
